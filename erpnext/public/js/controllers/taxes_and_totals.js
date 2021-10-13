383c8662fc701ac4263cda681cc22f144e36bc1e
Neeraj <60258936+neerajvkn@users.noreply.github.com>
2021-10-13 17:36:56 +0530

    fix : Updated write off amount calculation
    udpated write off amount calculation func to use rounded total if it is present else use grand total

--- erpnext/public/js/controllers/taxes_and_totals.js
+++ erpnext/public/js/controllers/taxes_and_totals.js
@@ -767,7 +767,8 @@ erpnext.taxes_and_totals = erpnext.payments.extend({
 
 	calculate_write_off_amount: function(){
 		if(this.frm.doc.paid_amount > this.frm.doc.grand_total){
-			this.frm.doc.write_off_amount = flt(this.frm.doc.grand_total - this.frm.doc.paid_amount
+			var grand_total = this.frm.doc.rounded_total || this.frm.doc.grand_total;
+			this.frm.doc.write_off_amount = flt(grand_total - this.frm.doc.paid_amount
 				+ this.frm.doc.change_amount, precision("write_off_amount"));
 
 			this.frm.doc.base_write_off_amount = flt(this.frm.doc.write_off_amount * this.frm.doc.conversion_rate,
